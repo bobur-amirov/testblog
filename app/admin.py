6f444977ce4de647df8ef3477e078b9b85850152
b.amirov <amirovbobur52@gmail.com>
2023-02-17 20:28:50 +0500

    first

---
+++ app/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from .models import Category, Tag, Blog, Comment
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+
+
+
+admin.site.register(Tag)
+admin.site.register(Blog)
+admin.site.register(Comment)
---
+++ user/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput(
+        attrs={'class': "form-control", 'placeholder': "Password"}
+    ))
+    password2 = forms.CharField(label="Password confrim", widget=forms.PasswordInput(
+        attrs={'class': "form-control", 'placeholder': "Password confirm"}
+    ))
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'password1',
+            'password2',
+        ]
+        widgets = {
+            'username': forms.TextInput(attrs={'class': "form-control"}),
+            'email': forms.TextInput(attrs={'class': "form-control"}),
+            'first_name': forms.TextInput(attrs={'class': "form-control"}),
+            'last_name': forms.TextInput(attrs={'class': "form-control"}),
+        }
